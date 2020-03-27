45ef1f76b64fa7ed315c3d69d3f6aa75e8d2f290
Javier <jduarte@caltech.edu>
2020-03-27 05:56:23 +0100

    update

--- models_3outputs.py
+++ models_3outputs.py
@@ -1,6 +1,6 @@
 import keras
 from keras.models import Model
-from keras.layers import Input, Dense, BatchNormalization, Dropout, Lambda
+from keras.layers import Input, Dense, BatchNormalization, Dropout, Lambda, Activation
 from keras.backend import slice
 
 def dense(ninputs, noutputs):
@@ -8,33 +8,22 @@ def dense(ninputs, noutputs):
     inputs = Input(shape=(ninputs,), name = 'input')  
     #x = BatchNormalization(name='bn_1')(inputs)
     x = Dense(64, name = 'dense_1', activation='relu')(inputs)
-    x = Dropout(rate=0.1)(x)
+    #x = Dropout(rate=0.1)(x)
+    #x = BatchNormalization(name='bn_2')(x)
     x = Dense(32, name = 'dense_2', activation='relu')(x)
-    x = Dropout(rate=0.1)(x)
+    #x = Dropout(rate=0.1)(x)
+    #x = BatchNormalization(name='bn_3')(x)
     x = Dense(32, name = 'dense_3', activation='relu')(x)
-    x = Dropout(rate=0.1)(x)
-    
-    outputs0 = Dense(1, name = 'output0', activation='linear')(x)
+    #x = Dropout(rate=0.1)(x)
+    #x = BatchNormalization(name='bn_4')(x)
+    outputs = Dense(noutputs, name = 'output', activation='linear')(x)
 
-    y = Dense(64, name = 'dense_4', activation='softmax')(inputs)
-    y = Dropout(rate=0.1)(y)
-    y = Dense(32, name = 'dense_5', activation='softmax')(y)
-    y = Dropout(rate=0.1)(y)
-    y = Dense(32, name = 'dense_6', activation='softmax')(y)
-    y = Dropout(rate=0.1)(y)
+    #outputs0 = Lambda(lambda x: slice(x, (0, 0), (-1,  1)))(outputs)
+    #outputs1 = Lambda(lambda x: slice(x, (0, 1), (-1, -1)))(outputs)
+    #outputs1 = Activation('softmax')(outputs1)
 
-    outputs1 = Dense(1, name = 'output1', activation='linear')(y)
-
-    z = Dense(64, name = 'dense_7', activation='softmax')(inputs)
-    z = Dropout(rate=0.1)(z)
-    z = Dense(32, name = 'dense_8', activation='softmax')(z)
-    z = Dropout(rate=0.1)(z)
-    z = Dense(32, name = 'dense_9', activation='softmax')(z)
-    z = Dropout(rate=0.1)(z)
-
-    outputs2 = Dense(1, name = 'output2', activation='linear')(z)
-
-    keras_model = Model(inputs=inputs, outputs=[outputs0, outputs1, outputs2])
+    #keras_model = Model(inputs=inputs, outputs=[outputs0, outputs1])
+    keras_model = Model(inputs=inputs, outputs=outputs)
 
     return keras_model
 
--- train_3outputs.py
+++ train_3outputs.py
@@ -1,23 +1,34 @@
 import tensorflow as tf
-from tensorflow.python.ops import math_ops
-from tensorflow.python import ops
 import keras
 import numpy as np
 import tables
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 from sklearn.metrics import roc_curve, auc
 from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 import matplotlib.pyplot as plt
 import argparse
 from models_3outputs import dense
 import math
 import keras.backend as K
 
+
+def delta_phi(phi1, phi2):
+    return np.mod(phi1 - phi2 + np.pi, 2*np.pi) - np.pi
+
 def huber_loss(y_true, y_pred, delta=1.0):
     error = y_pred - y_true
     abs_error = K.abs(error)
     quadratic = K.minimum(abs_error, delta)
     linear = abs_error - quadratic
+    return 0.5 * K.square(quadratic) + delta * linear
+
+def huber_loss_relative(y_true, y_pred, delta=1.0):
+    rel_error = K.abs((y_true - y_pred) / K.clip(K.abs(y_true),
+                                                K.epsilon(),
+                                                 None))
+    quadratic = K.minimum(rel_error, delta)
+    linear = rel_error - quadratic
     return 0.5 * K.square(quadratic) + delta * linear
 
 def mean_absolute_relative_error(y_true, y_pred):
@@ -37,6 +48,15 @@ def mean_squared_relative_error(y_true, y_pred):
                                             K.epsilon(),
                                             None))
     return K.mean(diff, axis=-1)
+
+def mean_squared_percentage_error(y_true, y_pred):
+    if not K.is_tensor(y_pred):
+        y_pred = K.constant(y_pred)
+    y_true = K.cast(y_true, y_pred.dtype)
+    diff = K.square((y_true - y_pred) / K.clip(K.square(y_true),
+                                            K.epsilon(),
+                                            None))
+    return 100.*K.mean(diff, axis=-1)
 
 def get_features_targets(file_name, features, targets):
     # load file
@@ -61,13 +81,14 @@ def get_features_targets(file_name, features, targets):
 
 def main(args):
     file_path = 'input_MET.h5'
-    features = ['L1CHSMet_pt', 'L1CHSMet_phi',
+    features = ['L1PuppiMet_pt', 'L1PuppiMet_phi',
+                'L1CHSMet_pt', 'L1CHSMet_phi',
                 'L1CaloMet_pt', 'L1CaloMet_phi',
                 'L1PFMet_pt', 'L1PFMet_phi',
-                'L1PuppiMet_pt', 'L1PuppiMet_phi',
                 'L1TKMet_pt', 'L1TKMet_phi',
-                'L1TKV5Met_pt', 'L1TKV5Met_phi',
-                'L1TKV6Met_pt', 'L1TKV6Met_phi']
+                #'L1TKV5Met_pt', 'L1TKV5Met_phi',
+                #'L1TKV6Met_pt', 'L1TKV6Met_phi',
+    ]
 
     targets = ['genMet_pt', 'genMet_phi']
 
@@ -76,40 +97,20 @@ def main(args):
     nfeatures = feature_array.shape[1]
     ntargets = target_array.shape[1]
 
-	# Exclude met, phi = 0 events ,,#and MET < 50 GeV
-    event_zero = 0
-    skip = 0
-    for i in range(nevents_1):
-        if (feature_array[i,10] == 0 and feature_array[i,11] == 0) or (feature_array[i,12] ==0 and feature_array[i,13] ==0) or (target_array[i,0] < 50):
-            event_zero = event_zero + 1
-    feature_array_without0 = np.zeros((nevents_1 - event_zero, 14))
-    target_array_without0 = np.zeros((nevents_1 - event_zero, 2))
-    for i in range(nevents_1):
-        if (feature_array[i,10] == 0 and feature_array[i,11] == 0) or (feature_array[i,12] ==0 and feature_array[i,13] ==0) or (target_array[i,0] < 50):
-            skip = skip + 1
-            continue
-        feature_array_without0[i - skip,:] = feature_array[i,:]
-        target_array_without0[i - skip,:] = target_array[i,:]
-    print(feature_array_without0)
-    print(target_array_without0)
-    nevents = feature_array_without0.shape[0]
+    # Exclude puppi met, phi = 0 events and gen met < 50 GeV
+    mask = ( ((feature_array[:,0] == 0) & (feature_array[:,1] == 0)) | (target_array[:,0] < 50) )
+    feature_array = feature_array[~mask]
+    target_array = target_array[~mask]
 
-    # Split phi into cos^2 and sin^2
-    feature_array = np.zeros((nevents, 21))
-    target_array = np.zeros((nevents, 3))
+    nevents = feature_array.shape[0]
 
-    for i in range(7):
-        if i%3 == 0:
-            feature_array[:,3*i] = feature_array_without0[:,2*i]
-        if i%3 == 1:
-			feature_array[:,(3*i)+1] = np.square(np.cos(feature_array_without0[:,(2*i)+1]))
-        if i%3 == 2:
-			feature_array[:,(3*i)+2] = np.square(np.sin(feature_array_without0[:,(2*i)+1]))
+    # Split target phi into cos^2 and sin^2
+    new_target_array = np.zeros((nevents, 3))
+    new_target_array[:,0] = target_array[:,0]/feature_array[:,0]
+    new_target_array[:,1] = np.square(np.cos(target_array[:,1]))
+    new_target_array[:,2] = np.square(np.sin(target_array[:,1]))
 
-    target_array[:,0] = target_array_without0[:,0]
-    target_array[:,1] = np.square(np.cos(target_array_without0[:,1]))
-    target_array[:,2] = np.square(np.sin(target_array_without0[:,1]))
-
+    target_array = new_target_array[:,0:1]
 
     nfeatures = feature_array.shape[1]
     ntargets = target_array.shape[1]
@@ -128,78 +129,101 @@ def main(args):
     X_val = X[splits[1]:splits[2]]
     X_test = X[splits[0]:splits[1]]
 
+    print(X_test)
+
+    X_scaler = preprocessing.StandardScaler().fit(X_train)
+    X_train = X_scaler.transform(X_train)
+    X_val = X_scaler.transform(X_val)
+    X_test = X_scaler.transform(X_test)
+
     y_train = y[0:splits[0]]
     y_val = y[splits[1]:splits[2]]
     y_test = y[splits[0]:splits[1]]
 
+    print(y_test)
+
+    y_scaler =  preprocessing.StandardScaler().fit(y_train)
+    #y_train = y_scaler.transform(y_train)
+    #y_val = y_scaler.transform(y_val)
+    #y_test = y_scaler.transform(y_test)
 
     # Set parameters for weight function
-    number_of_interval = 100
-    mean = y_val.shape[0]/number_of_interval
-
-
-    # Devide interval
-    MET_interval = np.zeros(number_of_interval)
-    for i in range(y_val.shape[0]):
-        for j in range(number_of_interval):
-            if (5 * j <= y_val[i,0] < 5 * (j+1)):
-               MET_interval[j] = MET_interval[j]+1
-               
-    def weight_array_function_MET(y_true):
-        k = 0
-        for i in range(number_of_interval):
-            if 5 * i <= y_true < 5 * (j+1):
-                break
-            k = MET_interval[j]
-        if k == 0:
-            k = 1
-        return mean/k
-
-    weight_array_MET= np.zeros(y_test.shape[0])
-    for i in range(y_test.shape[0]):
-        weight_array_MET[i] = weight_array_function_MET(y_val[i,0])
-
-    def weight_loss_MET(y_true, y_pred):
-        return K.mean(math_ops.square((y_pred - y_true)*weight_array_MET), axis=-1)
-
-    def mean_squared_phi_error(y_true, y_pred):
-        error = tf.atan2(tf.sin(y_pred - y_true), tf.cos(y_pred - y_true)) - math.pi
-        return K.mean(math_ops.square(error), axis=-1)
+    #w_train = 1./(9.76272*np.exp(-2.04719e-02*y_train[:,0]))
+    w_train = np.ones_like(y_train[:,0])
 
     keras_model = dense(nfeatures, ntargets)
 
-    keras_model.compile(optimizer='adam', loss=['mean_squared_error', 'mean_squared_error', 'mean_squared_error'], 
-                        loss_weights = [1., 5000., 5000], metrics=['mean_absolute_error'])
+    #keras_model.compile(optimizer='adam', loss=['mean_absolute_percentage_error', 'mean_absolute_percentage_error'],
+    #                    loss_weights = [1., 0.], metrics=['mean_absolute_error','mean_squared_error'])
+
+    loss_name = 'mean_absolute_percentage_error'
+    loss = loss_name
+
+    keras_model.compile(optimizer='adam', loss=loss,
+                        metrics=['mean_absolute_error','mean_squared_error'])
     print(keras_model.summary())
 
     early_stopping = EarlyStopping(monitor='val_loss', patience=10)
     model_checkpoint = ModelCheckpoint('keras_model_best.h5', monitor='val_loss', save_best_only=True)
     callbacks = [early_stopping, model_checkpoint]
 
-    keras_model.fit(X_train, [y_train[:,0], y_train[:,1], y_train[:,2]], batch_size=1024, 
-					epochs=100, validation_data=(X_val, [y_val[:,0], y_val[:,1], y_val[:, 2]]), shuffle=True,
-                    callbacks = callbacks)
+    keras_model.fit(X_train, y_train, batch_size=1024, #[y_train[:,0], y_train[:,1:]], batch_size=1024, 
+                    epochs=100, validation_data=(X_val, y_val), shuffle=True, # [y_val[:,0], y_val[:,1:]]), shuffle=True,
+                    callbacks = callbacks, sample_weight = w_train)#[w_train, w_train])
 
     keras_model.load_weights('keras_model_best.h5')
     
     predict_test = keras_model.predict(X_test)
-    predict_test = np.concatenate(predict_test,axis=1)
+    #predict_test = np.concatenate(predict_test,axis=1)
+
+    print(X_test)
+    print(y_test)
+    print(predict_test)
+    X_test = X_scaler.inverse_transform(X_test)
+    #y_test = y_scaler.inverse_transform(y_test)
+    #predict_test = y_scaler.inverse_transform(predict_test)
+
+    print(X_test)
     print(y_test)
     print(predict_test)
 
-    def print_res(gen_met, predict_met, name='Met_res.pdf'):
-		rel_err = (predict_met - gen_met)#/np.clip(gen_met, 1e-6, None)
-		plt.figure()          
-		plt.hist(rel_err, bins=np.linspace(-1., 1., 50+1))
-		plt.xlabel("Rel. err.")
-		plt.ylabel("Events")
-		plt.figtext(0.25, 0.90,'CMS',fontweight='bold', wrap=True, horizontalalignment='right', fontsize=14)
-		plt.figtext(0.35, 0.90,'preliminary', style='italic', wrap=True, horizontalalignment='center', fontsize=14) 
-		plt.savefig(name)
-	
-    print_res(y_test[:,0], predict_test[:,0], name = 'MET_pt_abs.pdf')
-    print_res(y_test[:,1], predict_test[:,1], name = 'MET_cos_abs.pdf')
-    print_res(y_test[:,2], predict_test[:,2], name = 'MET_sin_abs.pdf')
+    def print_res(gen_met, predict_met, name='Met_res.pdf',phi=False):
+        if phi:
+            gen_phi = np.arctan2(np.sqrt(gen_met[:,1]), np.sqrt(gen_met[:,0]))
+            predict_phi = np.arctan2(np.sqrt(predict_met[:,1]), np.sqrt(predict_met[:,0]))
+            rel_err = delta_phi(predict_phi,gen_phi)
+        else:
+            rel_err = (predict_met - gen_met)/gen_met
+        bins=np.linspace(-1., 1., 50+1)
+        from scipy.stats import norm
+        (mu, sigma) = norm.fit(rel_err)
+        mean = np.mean(rel_err)
+        std = np.std(rel_err)
+        median = np.median(rel_err)
+        minus1sigma = np.quantile(rel_err,q=0.16)
+        plus1sigma = np.quantile(rel_err,q=0.84)
+        plt.hist(rel_err, bins=bins,alpha=0.5,label='mean=%.2f,std.=%.2f,median=%.2f,68%%=[%.2f,%.2f]'%(mean,std,median,minus1sigma,plus1sigma))
+        
+        bins=np.linspace(-1., 1., 500+1)
+        y = norm.pdf( bins, mu, sigma)
+        y /= sum(y) 
+        y *= 10.*rel_err.shape[0]
+        l = plt.plot(bins, y, '--', linewidth=2)
+
+    plt.figure()          	
+    print_res(y_test[:,0]*X_test[:,0], predict_test[:,0]*X_test[:,0], name = 'nnMET_res.pdf', phi=False)
+    #print_res(y_test[:,0]*X_test[:,0], X_test[:,0], name = 'puppiMET_res.pdf', phi=False)
+    plt.legend(loc='upper right')
+    plt.xlabel("Rel. err.")
+    plt.ylabel("Events")
+    plt.xlim(-1,1)
+    plt.ylim(0,600)
+    plt.figtext(0.20, 0.90,'CMS',fontweight='bold', wrap=True, horizontalalignment='right', fontsize=14)
+    plt.figtext(0.30, 0.90,'preliminary', style='italic', wrap=True, horizontalalignment='center', fontsize=14) 
+    plt.figtext(0.70, 0.90,loss_name,  wrap=True, horizontalalignment='center', fontsize=11) 
+    plt.savefig('nnMET_puppiMET_res.pdf')
+        
+    #print_res(y_test[:,1:], predict_test[:,1:], name = 'nnMET_phi_res.pdf',phi=True)
     
 
 if __name__ == "__main__":
