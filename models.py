91673d621be2d5009941d81a9e9ac7ed8a66b6ab
ExcitingSquid <syd2337@gmail.com>
2020-06-01 08:39:24 +0900

    apply b.n.

--- models.py
+++ models.py
@@ -5,14 +5,21 @@ from keras.backend import slice
 
 def dense(ninputs, noutputs):
 
-    inputs = Input(shape=(ninputs,), name = 'input')  
-    #x = BatchNormalization(name='bn_1')(inputs)
-    x = Dense(64, name = 'dense_1', activation='relu')(inputs)
+    inputs = Input(shape=(ninputs,), name = 'input')
+
+
+    x = BatchNormalization(name='bn_1')(inputs)
+    x = Dense(64, name = 'dense_1', activation='relu', kernel_initializer='glorot_uniform')(x)
     x = Dropout(rate=0.1)(x)
+
+
     x = Dense(32, name = 'dense_2', activation='relu')(x)
     x = Dropout(rate=0.1)(x)
+
+
     x = Dense(32, name = 'dense_3', activation='relu')(x)
     x = Dropout(rate=0.1)(x)
+
     
     outputs = Dense(noutputs, name = 'output', activation='linear')(x)
 
