5e60ea1f7437cab5273658a96742074ed7c9cd56
Javier M. Duarte <jduarte@ucsd.edu>
2021-07-22 09:34:36 -0700

    update

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ import tensorflow.keras.backend as K
 import tensorflow as tf
 from tensorflow import slice
 from tensorflow.keras import initializers
+import numpy as np
 
 def dense_embedding(n_features=6, n_features_cat=2, n_dense_layers=3, activation='relu', number_of_pupcandis=100, embedding_input_dim={0: 13, 1: 3}, emb_out_dim=8, with_bias=True, t_mode = 0):
 
@@ -27,17 +28,26 @@ def dense_embedding(n_features=6, n_features_cat=2, n_dense_layers=3, activation
 
     if t_mode == 0:
         x = GlobalAveragePooling1D(name='pool')(x)
-        x = Conv1D(2, kernel_size=1, name = 'output', activation='linear')(x)
+        x = Conv1D(2, kernel_size=1, name='output', activation='linear')(x)
 
     if t_mode == 1:
-        w = Conv1D(1, kernel_size=1, activation='linear', kernel_initializer=initializers.VarianceScaling(scale=0.02))(x)
-        x = Multiply()([w, pxpy])
         if with_bias:
-            b = Conv1D(2, kernel_size=1, activation='linear', kernel_initializer=initializers.VarianceScaling(scale=0.02))(x)
-            x = Add()([x, b])
+            b = Conv1D(2, kernel_size=1, name='met_bias', activation='linear', kernel_initializer=initializers.VarianceScaling(scale=0.02))(x)
+            pxpy = Add()([pxpy, b])
+        w = Conv1D(1, kernel_size=1, name='met_weight', activation='linear', kernel_initializer=initializers.VarianceScaling(scale=0.02))(x)
+        w = BatchNormalization(trainable=False, name='met_weight_minus_one', epsilon=False)(w)
+        x = Multiply()([w, pxpy])
+
         x = GlobalAveragePooling1D(name='output')(x)
     outputs = x
 
     keras_model = Model(inputs=inputs, outputs=outputs)
 
+    keras_model.get_layer('met_weight_minus_one').set_weights([np.array([1.]), np.array([-1.]), np.array([0.]), np.array([1.])])
+
     return keras_model
+
+model = dense_embedding(n_features=6, n_features_cat=2, n_dense_layers=3, activation='relu', number_of_pupcandis=100, embedding_input_dim={0: 13, 1: 3}, emb_out_dim=8, with_bias=False, t_mode = 1)
+model.summary()
+model.save('output/model.h5')
+
