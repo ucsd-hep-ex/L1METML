766ca3b828b87fff9ef0d97efe738b346d10b3f0
simon-poon <93788837+simon-poon@users.noreply.github.com>
2022-02-14 02:18:52 -0500

    Update DataGenerator.py

--- DataGenerator.py
+++ DataGenerator.py
@@ -98,12 +98,10 @@ class DataGenerator(tensorflow.keras.utils.Sequence):
         return kT
     
     def z(self, pti, ptj):
-        if pti == 0 and ptj == 0:
-            z = 0
-            return z
-        else:
             min_pt = np.minimum(pti[:,0:1],ptj[:,0:1])
             z = min_pt/(pti + ptj)
+            z[np.isnan(z)] = 0
+            z[np.isinf(z)] = 0
             return z
         
     def __data_generation(self, unique_files, starts, stops):
