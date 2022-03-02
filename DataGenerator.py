167ed8d91e83844aad480422d436c921e4e6b3bc
simon-poon <93788837+simon-poon@users.noreply.github.com>
2022-03-01 22:51:13 -0500

    Update DataGenerator.py

--- DataGenerator.py
+++ DataGenerator.py
@@ -110,7 +110,7 @@ class DataGenerator(tensorflow.keras.utils.Sequence):
     #    return z
     
     def m2(self, pi, pj):
-        m2 = (np.linalg.norm(pi+pj)) ** 2
+        m2 = (np.linalg.norm((pi+pj),axis=-1) ** 2
         return m2
     
     def __data_generation(self, unique_files, starts, stops):
@@ -160,8 +160,6 @@ class DataGenerator(tensorflow.keras.utils.Sequence):
                 pt2 = pt[:, sender, :]
                 p1 = p_vector[:, receiver, :]
                 p2 = p_vector[:, sender, :]
-                print(p1, p2)
-                print(p1.shape, p2.shape)
                 dR = self.deltaR(eta1, phi1, eta2, phi2)
                 m2 = self.m2(p1,p2)
                 #kT = self.kT(pt1,pt2,dR)
