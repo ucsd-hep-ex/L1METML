b22c155143886daa062ae2c279028b90b241ca17
simon-poon <93788837+simon-poon@users.noreply.github.com>
2022-03-31 06:39:25 -0400

    Update DataGenerator.py

--- DataGenerator.py
+++ DataGenerator.py
@@ -126,11 +126,11 @@ class DataGenerator(tensorflow.keras.utils.Sequence):
             count50 = 0
             count0 = 0
             for batch_idx in range(self.batch_size):
-                if (np.all(x[batch_idx,75,:] == np.zeros(19))):
+                if (np.all(self.X[batch_idx,75,:] == np.zeros(19))):
                     count75 += 1
-                elif (np.all(x[batch_idx,50,:] == np.zeros(19))):
+                elif (np.all(self.X[batch_idx,50,:] == np.zeros(19))):
                     count50 += 1
-                elif (np.all(x[batch_idx,0,:] == np.zeros(19))):
+                elif (np.all(self.X[batch_idx,0,:] == np.zeros(19))):
                     count0 += 1
             print(count75)
             print(count50)
