dcabae31315266d02d58d083bd4615ddbbf0bb95
simon-poon <93788837+simon-poon@users.noreply.github.com>
2022-04-19 04:18:23 -0400

    Update DataGenerator.py

--- DataGenerator.py
+++ DataGenerator.py
@@ -162,9 +162,9 @@ class DataGenerator(tensorflow.keras.utils.Sequence):
             pt = Xi[:,:,0:1]
             
             pt_ordered = np.sort(pt,axis=0)[::-1]
-            if pt == pt_ordered:
+            if np.array_equal(pt,pt_ordered) == True:
                 print("Sorted by pt")
-            elif pt != pt_ordered:
+            elif np.array_equal(pt,pt_ordered) != pt_ordered:
                 print("Not sorted by pt")
             
             px = Xp[:,:,0:1]
