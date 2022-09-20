7739c5d15f2a7f78a2523ac131cc40124a1da4d4
simon-poon <93788837+simon-poon@users.noreply.github.com>
2022-09-20 02:34:36 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,3 @@
-import keras
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Dense, Embedding, BatchNormalization, Dropout, Lambda, Conv1D, SpatialDropout1D, Concatenate, Flatten, Reshape, Multiply, Add, GlobalAveragePooling1D, Activation, Permute
 import tensorflow.keras.backend as K
@@ -39,9 +38,6 @@ def dense_embedding(n_features=6,
             name='embedding{}'.format(i_emb))(input_cat)
         embeddings.append(embedding)
 
-    #edges=100*99
-    #edge_feat = Input(shape=(edges, 3), name='edge_feat')
-    #inputs.append(edge_feat)
     # can concatenate all 3 if updated in hls4ml, for now; do it pairwise
     # x = Concatenate()([inputs_cont] + embeddings)
     emb_concat = Concatenate()(embeddings)
@@ -238,7 +234,7 @@ def graph_embedding(compute_ef, n_features=6,
                     edge_list=[]):
     n_dense_layers = len(units)
     name = 'met'
-
+    number_of_pupcandis = int(number_of_pupcandis/4)    # go from 100 puppi candidates for the whole region to 25 cands in one region 
     inputs_cont = Input(shape=(number_of_pupcandis, n_features-2), name='input_cont')
     pxpy = Input(shape=(number_of_pupcandis, 2), name='input_pxpy')
 
@@ -268,9 +264,7 @@ def graph_embedding(compute_ef, n_features=6,
     emb_concat = Concatenate()(embeddings)
     x = Concatenate()([inputs_cont, emb_concat])
 
-    N = number_of_pupcandis
     P = n_features+n_features_cat
-    Nr = N*(N-1)  # number of relations (edges)
 
     x = BatchNormalization()(x)
 
@@ -339,117 +333,3 @@ def graph_embedding(compute_ef, n_features=6,
     # keras_model.get_layer('scalars').set_weights([w_zeros,b_zeros])
 
     return keras_model
-
-class Distiller(Model):
-    def __init__(self, student, teacher):
-        super(Distiller, self).__init__()
-        self.teacher = teacher
-        self.student = student
-
-    def call(self, inputs):
-        outputs = self.student(inputs)
-        outputs = tf.reshape(outputs,[-1,2])
-        return outputs
-
-    def compile(
-        self,
-        optimizer,
-        metrics,
-        student_loss_fn,
-        distillation_loss_fn,
-        alpha=0.1,
-        temperature=3,
-    ):
-        """ Configure the distiller.
-
-        Args:
-            optimizer: Keras optimizer for the student weights
-            metrics: Keras metrics for evaluation
-            student_loss_fn: Loss function of difference between student
-                predictions and ground-truth
-            distillation_loss_fn: Loss function of difference between soft
-                student predictions and soft teacher predictions
-            alpha: weight to student_loss_fn and 1-alpha to distillation_loss_fn
-            temperature: Temperature for softening probability distributions.
-                Larger temperature gives softer distributions.
-        """
-        super(Distiller, self).compile(optimizer=optimizer, metrics=metrics)
-        self.student_loss_fn = student_loss_fn
-        self.distillation_loss_fn = distillation_loss_fn
-        self.alpha = alpha
-        self.temperature = temperature
-
-    def train_step(self, data):
-        # Unpack data
-        x, y = data
-        print(y)
-        print(np.shape(y))
-        #print(x)
-
-        # Forward pass of teacher
-        teacher_predictions = self.teacher(x, training=False)
-
-        with tf.GradientTape() as tape:
-            # Forward pass of student
-            student_predictions = self.student(x, training=True)
-
-            # Compute losses
-            #student_predictions = keras.layers.Reshape([256, 2])(y)
-            #print(y)
-            print("------")
-            print("y")
-            print(y)
-            #student_predictions = keras.layers.Reshape([1,2])(student_predictions)
-            #y = keras.layers.Reshape([1, 2])(y)
-            print("y reshape")
-            print(y)
-
-            student_loss = self.student_loss_fn(y, student_predictions)
-
-            # Compute scaled distillation loss from https://arxiv.org/abs/1503.02531
-            # The magnitudes of the gradients produced by the soft targets scale
-            # as 1/T^2, multiply them by T^2 when using both hard and soft targets.
-            distillation_loss = (
-                self.distillation_loss_fn(
-                    tf.nn.softmax(teacher_predictions / self.temperature, axis=1),
-                    tf.nn.softmax(student_predictions / self.temperature, axis=1),
-                )
-                * self.temperature**2
-            )
-
-            loss = self.alpha * student_loss + (1 - self.alpha) * distillation_loss
-
-        # Compute gradients
-        trainable_vars = self.student.trainable_variables
-        gradients = tape.gradient(loss, trainable_vars)
-
-        # Update weights
-        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
-
-        # Update the metrics configured in `compile()`.
-        self.compiled_metrics.update_state(y, student_predictions)
-
-        # Return a dict of performance
-        results = {m.name: m.result() for m in self.metrics}
-        results.update(
-            {"student_loss": student_loss, "distillation_loss": distillation_loss}
-        )
-        return results
-
-    def test_step(self, data):
-        # Unpack the data
-        x, y = data
-
-        # Compute predictions
-        y_prediction = self.student(x, training=False)
-
-        # Calculate the loss
-        student_loss = self.student_loss_fn(y, y_prediction)
-
-        # Update the metrics.
-        self.compiled_metrics.update_state(y, y_prediction)
-
-        # Return a dict of performance
-        results = {m.name: m.result() for m in self.metrics}
-        results.update({"student_loss": student_loss})
-        return results
