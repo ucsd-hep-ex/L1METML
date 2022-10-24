fc4b7f815429914bb61c02ea7364bcedd3b942df
simon-poon <93788837+simon-poon@users.noreply.github.com>
2022-10-23 21:38:22 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
-from tkinter import FALSE
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Dense, Embedding, BatchNormalization, Dropout, Lambda, Conv1D, SpatialDropout1D, Concatenate, Flatten, Reshape, Multiply, Add, GlobalAveragePooling1D, Activation, Permute, Add, LeakyReLU, Layer
+from tensorflow.keras.layers import Input, Dense, Embedding, BatchNormalization, Dropout, Lambda, Conv1D, SpatialDropout1D, Concatenate, Flatten, Reshape, Multiply, Add, GlobalAveragePooling1D, Activation, Permute
 import tensorflow.keras.backend as K
 import tensorflow as tf
 from tensorflow import slice
@@ -153,156 +152,6 @@ def assign_matrices(N, Nr):
     return Rs, Rr
 
 
-def S(tensor, threshold):
-    x = tf.where(tf.math.greater_equal(tensor, T), 1, 0)
-    return x
-
-
-def node_select(compute_ef, n_features=6,
-                n_features_cat=2,
-                activation='relu',
-                number_of_pupcandis=100,
-                embedding_input_dim={0: 13, 1: 3},
-                emb_out_dim=8,
-                units=[64, 32, 16]):
-
-    N = number_of_pupcandis
-    Nr = N*(N-1)
-
-    inputs_cont = Input(shape=(number_of_pupcandis, n_features-2), name='input_cont')
-    pxpy = Input(shape=(number_of_pupcandis, 2), name='input_pxpy')
-
-    embeddings = []
-    inputs = [inputs_cont, pxpy]
-    for i_emb in range(n_features_cat):
-        input_cat = Input(shape=(number_of_pupcandis, ), name='input_cat{}'.format(i_emb))
-        inputs.append(input_cat)
-        embedding = Embedding(
-            input_dim=embedding_input_dim[i_emb],
-            output_dim=emb_out_dim,
-            embeddings_initializer=initializers.RandomNormal(
-                mean=0,
-                stddev=0.4/emb_out_dim),
-            name='embedding{}'.format(i_emb))(input_cat)
-        embeddings.append(embedding)
-
-    emb_concat = Concatenate()(embeddings)
-    x = Concatenate()([inputs_cont, emb_concat])
-    x = BatchNormalization()(x)  # (batch,nodes,features)
-
-    # Swap axes of input data (batch,nodes,features) -> (batch,features,nodes)
-    x = Permute((2, 1), input_shape=x.shape[1:])(x)
-
-    # node selection
-
-    F_prime = 6
-    wx = Dense(F_prime, use_bias=False, trainable=True, name='W')(x)  # (nodes,F')
-
-    # Swap axes of input data (batch,nodes,features) -> (batch,F',nodes)
-    x = Permute((2, 1), input_shape=x.shape[1:])(x)
-
-    ORs = Dense(Nr, use_bias=False, trainable=False, name='sending'.format(name))(wx)   # Neighborhood aggregation   (F', Nr)
-    ORr = Dense(N, use_bias=False, trainable=False, name='receiving'.format(name))(ORs)  # (F', N)
-
-    x = Permute((2, 1), input_shape=x.shape[1:])(ORr)  # (N, F')
-    p = Dense(1, activation='relu', use_bias=False, trainable=True, name='p')(x)  # (N,1), W0 weightmatrix
-
-    S = Lambda(S(p, T))  # node selection function
-
-    # Selective Aggregation and Feature Update
-
-    # alpha
-
-    # A matrix
-    A = Multiply(S, wx)
-    A = Multiply(alhpa, A)
-
-    keras_model = Model(inputs=inputs, outputs=w0)
-
-    Rs, Rr = assign_matrices(N, Nr)
-    keras_model.get_layer('sending').set_weights([np.transpose(Rr)])
-    keras_model.get_layer('receiving'.format(name)).set_weights([Rs])
-    return keras_model
-
-
-class multiply_e(Layer):
-    def __init__(self):
-        super(multiply_e, self).__init__()
-
-    def call(sef, inputs):
-        eij = inputs
-        outputs = tf.math.exp(eij)
-        return outputs
-
-
-class softmax(Layer):
-    def __init__(self):
-        super(softmax, self).__init__()
-
-    def call(self, inputs):
-        sum_e, eij = inputs     # sum_e: 1xN   eij = 1xNr
-        outputs = tf.math.divide(eij,sum_e)
-        return outputs
-
-class attention1(Layer):
-    def __init__(self, N, Nr, Fprime):
-        super(attention1, self).__init__()
-        self.Rs, self.Rr = assign_matrices(N, Nr)
-        self.Fprime = Fprime
-    def build(self, input_shape):
-        self.w = self.add_weight(
-                                shape=(self.Fprime, input_shape[-2]),   # F' x F
-                                initializer="random_normal",
-                                trainable=True, name='w'
-                                )
-        self.a = self.add_weight(    # 1 x 2F'
-                                shape=(1, 2*self.Fprime),   # F' x F
-                                initializer="random_normal",
-                                trainable=True, name='a'
-                                )
-    def call(self, inputs):
-        wh = tf.tensordot(inputs, self.w, axes=[1,1])  # F' x F  x  (B x F x N)  --> F' x N
-        whi = tf.tensordot(wh,self.Rr, axes=[1,0])   # (F' x N) x (N X Nr) ---> (F' x Nr)
-        whj = tf.tensordot(wh, self.Rs,axes=[1,0])
-        whi_whj = tf.concat([whi, whj], 1)   # ---> (2F' X Nr)
-        a_whi_whj = tf.tensordot(whi_whj, self.a, axes=[1,1])    #  (B x 2F' x Nr) x (1 x 2F') ---> (B x Nr x 1)
-        return a_whi_whj
-    def get_config(self):
-        config = super(attention1, self).get_config()
-        config.update({
-            'Rs': self.Rs,
-            'Rr': self.Rr,
-            'Fprime': self.Fprime})
-        return config
-
-class attention2(Layer):
-    def __init__(self, N, Nr):
-        super(attention2, self).__init__()
-        self.Rs2, self.Rr2 = assign_matrices(N, Nr)
-    def call(self, inputs):
-        exp = tf.math.exp(inputs)    # B x Nr x 1
-        exp_sum = tf.tensordot(exp, self.Rr2, axes = [1,1])   # (B x Nr x 1) x (N x Nr)  ---> (B x 1 x N)
-        exp_sum = tf.tensordot(exp_sum, self.Rr2, axes=1)    # (B x 1 x N) x (N x Nr)   ----> (B x 1 x Nr)
-        inputs_exp = tf.transpose(inputs,[0,2,1])
-        output = tf.math.divide(inputs_exp, exp_sum)
-        return output
-    def get_config(self):
-        config = super(attention2, self).get_config()
-        config.update({
-            'Rs2': self.Rs2,
-            'Rr2': self.Rr2})
-        return config
-
-class attn_mult(Layer):
-    def __init__(self):
-        super(attn_mult, self).__init__()
-    def call(self,inputs):
-        tensor = inputs[0]
-        attn = inputs[1]
-        outputs = tf.multiply(tensor, attn)
-        return outputs
-
-
 def graph_embedding(compute_ef, n_features=6,
                     n_features_cat=2,
                     activation='relu',
@@ -352,31 +201,6 @@ def graph_embedding(compute_ef, n_features=6,
     # Swap axes of input data (batch,nodes,features) -> (batch,features,nodes)
     x = Permute((2, 1), input_shape=x.shape[1:])(x)
 
-    attn1 = attention1(N, Nr, 8)(x)
-    attn1 = LeakyReLU()(attn1)
-    attn1 = attention2(N, Nr)(attn1)
-    attn2 = attention1(N, Nr, 8)(x)
-    attn2 = LeakyReLU()(attn2)
-    attn2 = attention2(N, Nr)(attn2)
-    
-
-
-
-    # Whi Whj
-    #wh = Dense(F, use_bias=False, trainable=True)(x)  # nodes, features  ---> nodes, F'  
-    #wh = Permute((2,1))(wh)  # nodes, F' ---> F', nodes
-    #r = Dense(Nr, use_bias=False, trainable=False, name="receiving")(wh)  # Receiving adjacency matrix    F', Nr
-    #s = Dense(Nr, use_bias=False, trainable=False, name="sending")(wh)  # Sending adjacency matrix      F', Nr
-    #eq3_concat = Concatenate(axis=1)([r,s])   # 2FxNr
-    #eq3 = Dense(1, activation='Leakyrelu', use_bias=False, trainable=True, name="a_vec")(eq3_concat)    # Nr x 1
-    ## eq3_vec = [a  b  c  d]
-    ##           [a  b  c  d]
-    ##           [a  b  c  d]
-    ##           [a  b  c  d]
-
-    #sum_e = Dense(N, use_bias=False, trainable=False, name="sending")   # N x 1
-    #norm =  Softmax()(sum_e, eq3)
-
     # Marshaling function
     ORr = Dense(Nr, use_bias=False, trainable=False, name='tmul_{}_1'.format(name))(x)  # Receiving adjacency matrix
     ORs = Dense(Nr, use_bias=False, trainable=False, name='tmul_{}_2'.format(name))(x)  # Sending adjacency matrix
@@ -385,16 +209,10 @@ def graph_embedding(compute_ef, n_features=6,
 
     # Edges MLP
     h = Permute((2, 1), input_shape=node_feat.shape[1:])(node_feat)
-    # feature selection scalars are generated from bias vectors from 'scalars' dense layer
-    #init_scl_array = np.ones([16 + num_of_edge_feat])
-    # init_scl_input = Dense(16, trainable=False, use_bias=False, name='scalars_init')(h)  # This line is is to set next layer's weights to zero so we get just the bias
-    #scl = Dense(16+num_of_edge_feat, trainable=True, activation='softmax', bias_initializer=initializers.Ones(), name='scalars')(init_scl_input)
     edge_units = [64, 32, 16]
     n_edge_dense_layers = len(edge_units)
     if compute_ef == 1:
         h = Concatenate(axis=2, name='concatenate_edge')([h, edge_feat])
-        # output is [batch, edges, node features + edge features]
-        # h = Multiply()([h,scl]) # multiply scalars with features
     for i_dense in range(n_edge_dense_layers):
         h = Dense(edge_units[i_dense], activation='linear', kernel_initializer='lecun_uniform')(h)
         h = BatchNormalization(momentum=0.95)(h)
@@ -403,10 +221,6 @@ def graph_embedding(compute_ef, n_features=6,
 
     # Transpose output and permutes columns 1&2
     out_e = Permute((2, 1))(out_e)
-    mult1 = attn_mult()([out_e, attn1])
-    mult2 = attn_mult()([out_e, attn2])
-    out_e = Concatenate(axis=1)([mult1, mult2])
-
 
     # Multiply edges MLP output by receiver nodes matrix Rr
     out_e = Dense(N, use_bias=False, trainable=False, name='tmul_{}_3'.format(name))(out_e)
@@ -436,12 +250,5 @@ def graph_embedding(compute_ef, n_features=6,
     keras_model.get_layer('tmul_{}_1'.format(name)).set_weights([Rr])
     keras_model.get_layer('tmul_{}_2'.format(name)).set_weights([Rs])
     keras_model.get_layer('tmul_{}_3'.format(name)).set_weights([np.transpose(Rr)])
-    #keras_model.get_layer('receiving').set_weights([Rr])
-    #keras_model.get_layer('receiving').set_weights([Rs])
-    #w_zeros = np.zeros((16, 16+num_of_edge_feat))
-    #b_zeros = np.ones((19))
-    #init_zeros = np.zeros((16, 16))
-    # keras_model.get_layer('scalars_init').set_weights([init_zeros])
-    # keras_model.get_layer('scalars').set_weights([w_zeros,b_zeros])
 
     return keras_model
