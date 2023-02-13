6846a625b40aa7505a06920713e48050c4af9410
simon-poon <93788837+simon-poon@users.noreply.github.com>
2023-02-12 22:08:58 -0500

    Update DataGenerator.py

--- DataGenerator.py
+++ DataGenerator.py
@@ -34,10 +34,16 @@ class DataGenerator(tensorflow.keras.utils.Sequence):
 
         self.h5files = []
         for ifile in list_files:
-            h5file_path = ifile.replace('.root', '.h5')
+            if 'singleneutrino' in ifile.lower():
+                h5file_path = ifile
+            else:
+                h5file_path = ifile.replace('.root', '.h5')
             if not os.path.isfile(h5file_path):
+                if 'singleneutrino' in h5file_path.lower():
+                    raise ValueError("No single neutrino file found")
                 os.system(f'python convertNanoToHDF5_L1triggerToDeepMET.py -i {ifile} -o {h5file_path}')
             self.h5files.append(h5file_path)
+        print(self.h5files)
         for i, file_name in enumerate(self.h5files):
             with h5py.File(file_name, "r") as h5_file:
                 self.open_files.append(h5_file)
