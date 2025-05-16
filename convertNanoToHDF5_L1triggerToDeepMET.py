ca84c032cba63bdd6d812687f1e9a21606c503fb
dprim7 <daniel.primosch@cern.ch>
2025-05-16 15:46:23 -0700

    changed nano to hdf5 converter to handle up to 128 PUPPI cands

--- convertNanoToHDF5_L1triggerToDeepMET.py
+++ convertNanoToHDF5_L1triggerToDeepMET.py
@@ -78,7 +78,7 @@ if not opt.data:
 upfile = uproot.open(opt.input)
 tree = upfile['Events'].arrays(varList, entry_stop=opt.maxevents)
 # general setup
-maxNPuppi = 100
+maxNPuppi = 128
 nFeatures = 8
 maxEntries = len(tree['nL1PuppiCands'])
 # input Puppi candidates
